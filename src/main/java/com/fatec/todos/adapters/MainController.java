d5d1ae08e59245bbc531dccc3400a1bf5ea5c6ba
PHCJ <paulohenriquecardosodjesus@gmail.com>
2022-05-20 08:37:43 -0300

    index

--- src/main/java/com/fatec/todos/adapters/MainController.java
+++ src/main/java/com/fatec/todos/adapters/MainController.java
@@ -2,6 +2,7 @@ package com.fatec.todos.adapters;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -9,4 +10,8 @@ public class MainController {
 	public String AcessarPrincipal() {
 		return "admin/home";
 	}	
+	@GetMapping("/")
+	public ModelAndView home() {
+		return new ModelAndView("index");
+	}
 }
